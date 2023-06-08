af7c63d8702e790099ed9544b8ee9152acbf20a8
Idris Noor <idreesnoor150@gmail.com>
2023-06-08 10:21:39 +0300

    second update

--- TODOLIST/src/App.jsx
+++ TODOLIST/src/App.jsx
@@ -1,67 +1,10 @@
-import React, { useState, useEffect } from 'react';
-
+import './App.css';
+import { TodoWrapper } from './components/TodoWrapper';
 
 function App() {
-  const [todos, setTodos] = useState([]);
-  const [newTodo, setNewTodo] = useState('');
-
-  useEffect(() => {
-    fetchTodos();
-  }, []);
-
-  const fetchTodos = async () => {
-    try {
-      const response = await fetch('http://localhost:9292/todos');
-      const data = await response.json();
-      setTodos(data);
-    } catch (error) {
-      console.error('Error fetching todos:', error);
-    }
-  };
-
-  const addTodo = async () => {
-    try {
-      const response = await fetch('http://localhost:9292/todos', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ title: newTodo }),
-      });
-      const data = await response.json();
-      setTodos([...todos, data]);
-      setNewTodo('');
-    } catch (error) {
-      console.error('Error adding todo:', error);
-    }
-  };
-
-  const deleteTodo = async (id) => {
-    try {
-      await fetch(`http://localhost:9292/todos/${id}`, {
-        method: 'DELETE',
-      });
-      setTodos(todos.filter((todo) => todo.id !== id));
-    } catch (error) {
-      console.error('Error deleting todo:', error);
-    }
-  };
-
   return (
-    <div>
-      <h1>Todo List</h1>
-      <input
-        type="text"
-        value={newTodo}
-        onChange={(e) => setNewTodo(e.target.value)}
-      />
-      <button onClick={addTodo}>Add Todo</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>
-            {todo.title}
-            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
-          </li>
-        ))}
-      </ul>
+    <div className="App">
+      <TodoWrapper  />
     </div>
   );
 }
